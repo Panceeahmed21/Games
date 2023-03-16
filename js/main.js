762ae103e969af6b87d6c24eca59b0185570c7bb
Panceeahmed21 <panceeahmed22@gamil.com>
2023-03-16 12:08:18 +0200

    changes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
+
 import  { apiData } from './UI.js'
+import  { games } from './games.js'
+$(document).ready(function(){
+$(".lds-roller").fadeOut(1000,function(){
+    $(".loading-page").fadeOut(1000,function(){
+        $("body").css("overflow","auto")
+    })
+})
+
 let dataFromAPI =new apiData ()
 dataFromAPI.displayGames()
  dataFromAPI.displayDetails()
 
-import  { games } from './games.js'
+
 let gamesData =new games ()
 gamesData.getCategory()
 
+})
